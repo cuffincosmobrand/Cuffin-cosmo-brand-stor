6b1f10ce7204b8c48b90abc7cbb2feb83831616e
cuffincosmobrand <ezzatmahmoudelking2002@gmail.com>
2026-09-10 01:08:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,16 @@ image: "images/product.jpg",
 description: "Product description"
 }
 */
-const products = [];
+const products = [
+    {
+        id: 1,
+        name: "passion baby fresh baby cologne",
+        category: "skin care",
+        price: 85,
+        image: "images/passion baby fresh baby cologne.jpg",
+        description: "passion baby fresh baby cologne"
+    }
+];
 /* =========================================
 CART
 ========================================= */
